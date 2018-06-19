4ece4986641bdb664e536fa37080dd716650e5de
PisethSlash <pisethmspore@gmail.com>
2018-06-19 15:26:36 +0700

    Add Neural Network

--- main.py
+++ main.py
@@ -6,44 +6,50 @@ from slashml.machine_learning import MachineLearning
 from slashml.preprocessing.preprocessing_data import Preprocessing
 from slashml.utils.file_util import FileUtil
 
+#
 if __name__ == "__main__":
 
-    config = {
-        'root': '/Users/lion/Documents/py-workspare/openml/slash-ml',
-        'text_dir': 'data/dataset/text',
-        'dataset': 'data/dataset/matrix',
-        'bag_of_words': 'data/dataset/bag_of_words',
-        'train_model': 'data/naive_bayes.model',
-        # 'mode': 'unicode'
-    }
-    # with open('config/env.sample.json') as json_data:
-    #   config = json.load(json_data)
-    # preposessing
-    prepro = Preprocessing(**config)
-    #dataset_matrix = prepro.loading_data(config['text_dir'], 'doc_freq', 'top', 25)
+  config = {
+    'root': '/Users/lion/Documents/py-workspare/openml/slash-ml',
+    'text_dir': 'data/dataset/text',
+    'dataset': 'data/dataset/matrix',
+    'bag_of_words': 'data/dataset/bag_of_words',
+    'train_model': 'data/naive_bayes.model',
+    # 'mode': 'unicode'
+  }
 
 
-    ml = MachineLearning(**config)
+  # with open('config/env.sample.json') as json_data:
+  #   config = json.load(json_data)
+  # preposessing
+  prepro = Preprocessing(**config)
+  #dataset_matrix = prepro.loading_data(config['text_dir'], 'doc_freq', 'top', 25)
 
-    #load dataset from file (feature data)
-    filename = "doc_freq_25.csv"
-    dataset_path = FileUtil.dataset_path(config, filename)
-    dataset_sample = FileUtil.load_csv(dataset_path, use_numpy=True)
-    dataset_sample = prepro.normalize_dataset(dataset_sample)
+  ml = MachineLearning(**config)
 
-    # dataset -> train, test
-    # training_set, test_set = ml.split_dataset(dataset_sample, 5)
-    training_set, test_set = ml.train_test_split(dataset_sample, 5)
-    # choose your algorithm
-    algo = ml.NiaveBayes()
-    algo = ml.DecisionTree(criterion='gini', prune='depth', max_depth=30, min_criterion=0.05)
-    # train or load model
-    model = algo.train(training_set)
-    # model = algo.load_model()
-    # make a prediction
-    predictions = algo.predict(model, test_set)
-    acc = ml.accuracy(predictions,test_set)
+  #load dataset from file (feature data)
+  filename = "doc_freq_25.csv"
+  dataset_path = FileUtil.dataset_path(config, filename)
+  dataset_sample = FileUtil.load_csv(dataset_path, use_numpy=True)
+  #dataset_sample = prepro.normalize_dataset(dataset_sample)
 
-    print('training_set', len(training_set))
-    print('predictions, prediction_details', predictions, acc)
-    print('label', ml.to_label(predictions))
+  # dataset -> train, test
+  # training_set, test_set = ml.split_dataset(dataset_sample, 5)
+  training_set, test_set = ml.train_test_split(dataset_sample, 5)
+  # choose your algorithm
+  #algo = ml.NiaveBayes()
+  #algo = ml.DecisionTree(criterion='gini', prune='depth', max_depth=30, min_criterion=0.05)
+  algo = ml.NeuralNetwork(hidden_layer_sizes=(250, 100), learning_rate=0.012, momentum=0.5, random_state=0, \
+    max_iter=200, activation='tanh')
+  # train or load model
+  #model = algo.train(training_set)
+  model = algo.load_model()
+  # make a prediction
+  predictions = algo.predict(model, test_set)
+  # Prediction accuracy
+  acc = ml.accuracy(predictions, test_set)
+
+  print('training_set', len(training_set))
+  print('predictions, prediction_details', predictions, acc)
+  print('label', ml.to_label(predictions))
+  #demo(config)
--- slashml/utils/file_util.py
+++ slashml/utils/file_util.py
@@ -1,221 +1,207 @@
 """
-
 """
-
 import csv
 import os
 import pickle
 import zipfile
 import shutil
 
+from numpy import genfromtxt
 
 class FileUtil(object):
-    """ File Utilities for read and write file
+  """ File Utilities for read and write file
+  """
+
+  @staticmethod
+  def read_file(filename, file_extension):
+    """ Read file
+        return: dataset as list
     """
 
-    @staticmethod
-    def read_file(filename, file_extension):
-        """ Read file
+    _data_matrix = []
+    if file_extension == 'CSV':
+        _data_matrix = FileUtil.load_csv(filename)
 
-            return
-                dataset as list
-        """
+    return _data_matrix
 
-        _data_matrix = []
-        if file_extension == 'CSV':
-            _data_matrix = FileUtil.load_csv(filename)
+  @staticmethod
+  def load_csv(filename, use_numpy=False):
+    """ Read data from csv file using python or numpy lib
+    """
 
-        return _data_matrix
+    if not use_numpy:
+        return FileUtil.load_csv_py(filename)
+    else:
+        return FileUtil.load_csv_np(filename)
 
-    @staticmethod
-    def load_csv(filename, use_numpy=False):
-        """ Read data from csv file using python or numpy lib
-        """
+  @staticmethod
+  def load_csv_py(filename):
+    """ Read data from csv file.
+    """
 
-        if not use_numpy:
-            return FileUtil.load_csv_py(filename)
-        else:
-            return FileUtil.load_csv_np(filename)
+    file_obj = open(filename, "rU")
+    file_csv = csv.reader(file_obj)
+    _dataset = list(file_csv)
+    for index, subset in enumerate(_dataset):
+        _dataset[index] = [float(x) for x in subset]
 
+    file_obj.close()
 
-    @staticmethod
-    def load_csv_py(filename):
-        """ Read data from csv file.
-        """
-
-        file_obj = open(filename, "rU")
-        file_csv = csv.reader(file_obj)
-        _dataset = list(file_csv)
-        for index, subset in enumerate(_dataset):
-            _dataset[index] = [float(x) for x in subset]
-
-        file_obj.close()
-
-        return _dataset
+    return _dataset
 
 
-    @staticmethod
-    def load_csv_np(filename):
-        """ Load data from a text file
+  @staticmethod
+  def load_csv_np(filename):
+    """ Load data from a text file
+    When spaces are used as delimiters, or when no delimiter has been given as input\
+    , there should not be any missing data between two fields.
+    When the variables are named (either by a flexible dtype or with names\
+    , there must not be any header in the file (else a ValueError exception is raised).
+    Individual values are not stripped of spaces by default. When using a custom converter
+    , make sure the function does remove spaces
+    """
 
-        When spaces are used as delimiters, or when no delimiter has been given as input\
-        , there should not be any missing data between two fields.
-        When the variables are named (either by a flexible dtype or with names\
-        , there must not be any header in the file (else a ValueError exception is raised).
-        Individual values are not stripped of spaces by default. When using a custom converter
-        , make sure the function does remove spaces
-        """
+    # Data read from the text file. If usemask is True, this is a masked array.
+    _dataset = genfromtxt(filename, delimiter=',')
 
-        from numpy import genfromtxt
+    return _dataset
 
-        # Data read from the text file. If usemask is True, this is a masked array.
-        _dataset = genfromtxt(filename, delimiter=',')
-
-        return _dataset
+  @staticmethod
+  def dataset_path(config, filename):
+    """ path to file
+    """
+    path = os.path.join(os.getcwd(), config['dataset'], filename)
+    return path
 
 
-    @staticmethod
-    def dataset_path(config, filename):
-        """ path to file
-        """
-        path = os.path.join(os.getcwd(), config['dataset'], filename)
+  @staticmethod
+  def path_to_file(dirname, filename):
+    """ path to file
+    """
 
-        return path
+    path = os.path.join(os.getcwd(), dirname, filename)
+    return path
 
-    @staticmethod
-    def path_to_file(dirname, filename):
-        """ path to file
-        """
+  @staticmethod
+  def join_path(dirname):
+    """ path to file
+    """
 
-        path = os.path.join(os.getcwd(), dirname, filename)
+    path = os.path.join(os.getcwd(), dirname)
+    return path
 
-        return path
+  @staticmethod
+  def save_pickle_dataset(pickle_filename, dataset):
+    """ The pickle module implements binary protocols
+    for serializing and de-serializing a Python object structure.
+    """
 
-    @staticmethod
-    def join_path( dirname):
-        """ path to file
-        """
-        path = os.path.join(os.getcwd(), dirname)
+    path_to_pickle = os.path.join(os.getcwd(), pickle_filename)
+    with open(path_to_pickle, 'wb') as handle:
+        pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        return path
+    return True
 
-    @staticmethod
-    def save_pickle_dataset( pickle_filename, dataset):
-        """ The pickle module implements binary protocols
-        for serializing and de-serializing a Python object structure.
 
-        """
-        path_to_pickle = os.path.join(os.getcwd(), pickle_filename)
+  @staticmethod
+  def save_model(config, model):
+    """ The pickle module implements binary protocols
+    for serializing and de-serializing a Python object structure.
+    """
+
+    #s_model = {'hello': 'world'}
+    path_to_pickle = os.path.join(os.getcwd(), config['train_model'])
+    try:
         with open(path_to_pickle, 'wb') as handle:
-            pickle.dump(dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+            pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    except pickle.PickleError as error:
+        raise Exception(error)
+    else:
         return True
 
-    @staticmethod
-    def save_model(config, model):
-        """ The pickle module implements binary protocols
-        for serializing and de-serializing a Python object structure.
 
-        """
+  @staticmethod
+  def load_model(config):
+    """ Read .pickle file
+    """
 
-        #s_model = {'hello': 'world'}
-        path_to_pickle = os.path.join(os.getcwd(), config['train_model'])
-        try:
-            with open(path_to_pickle, 'wb') as handle:
-                pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        except pickle.PickleError as error:
-            raise Exception(error)
-        else:
-            return True
+    path_to_pickle = os.path.join(os.getcwd(), config['train_model'])
+    try:
+        with open(path_to_pickle, 'rb') as handle:
+            model = pickle.load(handle)
+    except pickle.UnpicklingError as error:
+        raise Exception(error)
+    else:
+        return model
 
-    @staticmethod
-    def load_model(config):
-        """ Read .pickle file
-        """
+  @staticmethod
+  def load_pickle(pickle_filename):
+    """ Read .pickle file
+    """
 
-        path_to_pickle = os.path.join(os.getcwd(), config['train_model'])
-        try:
-            with open(path_to_pickle, 'rb') as handle:
-                model = pickle.load(handle)
-        except pickle.UnpicklingError as error:
-            raise Exception(error)
-        else:
-            return model
+    path_to_pickle = os.path.join(os.getcwd(), pickle_filename)
+    try:
+        with open(path_to_pickle, 'rb') as handle:
+            model = pickle.load(handle)
+    except pickle.UnpicklingError as error:
+        raise Exception(error)
+    else:
+        return model
 
-    @staticmethod
-    def load_pickle(pickle_filename):
-        """ Read .pickle file
-        """
+  @staticmethod
+  def extract_zipfile(path_to_zipefile, dest_path):
+    """ Extract zipfile sent from client
+    Store temporarily in server
+    """
 
-        path_to_pickle = os.path.join(os.getcwd(), pickle_filename)
-        try:
-            with open(path_to_pickle, 'rb') as handle:
-                model = pickle.load(handle)
-        except pickle.UnpicklingError as error:
-            raise Exception(error)
-        else:
-            return model
-
-    @staticmethod
-    def extract_zipfile(path_to_zipefile, dest_path):
-        """ Extract zipfile sent from client
-        Store temporarily in server
-        """
-
-        try:
-            with zipfile.ZipFile(path_to_zipefile) as opened_rar:
-                opened_rar.extractall(dest_path)
-            #opened_rar = zipfile.ZipFile(path_to_zipefile)
-        except OSError as error:
-            raise Exception(error)
-        else:
-            return True
-
-    @staticmethod
-    def move_file(source, destination):
-        """ This function is used to move file from source to destination
-        """
-
-        try:
-            shutil.move(source, destination)
-        except OSError as error:
-            print('error %s' % error)
-            return False
-
+    try:
+        with zipfile.ZipFile(path_to_zipefile) as opened_rar:
+            opened_rar.extractall(dest_path)
+        #opened_rar = zipfile.ZipFile(path_to_zipefile)
+    except OSError as error:
+        raise Exception(error)
+    else:
         return True
 
-    @staticmethod
-    def remove_file(path, ignore_errors=False, onerror=None):
-        """ Delete an entire directory tree; path must point to a directory \
-        (but not a symbolic link to a directory). If ignore_errors is true, errors \
-        resulting from failed removals will be ignored; if false or omitted, \
-        such errors are handled by calling a handler specified by onerror or, \
-        if that is omitted, they raise an exception.
-        """
+  @staticmethod
+  def move_file(source, destination):
+    """ This function is used to move file from source to destination
+    """
 
-        ## check if a file exists on disk ##
-        ## if exists, delete it else show message on screen ##
-        if os.path.exists(path):
-            if os.path.isfile(path):
-                try:
-                    os.remove(path)
-                except OSError as error:
-                    print("Error: %s - %s." % (error.filename, error.strerror))
-            else:
-                try:
-                    shutil.rmtree(path, ignore_errors, onerror)
-                except OSError as error:
-                    print('error %s' % error)
-                    return False
+    try:
+        shutil.move(source, destination)
+    except OSError as error:
+        print('error %s' % error)
+        return False
+
+    return True
+
+
+  @staticmethod
+  def remove_file(path, ignore_errors=False, onerror=None):
+    """ Delete an entire directory tree; path must point to a directory \
+    (but not a symbolic link to a directory). If ignore_errors is true, errors \
+    resulting from failed removals will be ignored; if false or omitted, \
+    such errors are handled by calling a handler specified by onerror or, \
+    if that is omitted, they raise an exception.
+    """
+
+    ## check if a file exists on disk ##
+    ## if exists, delete it else show message on screen ##
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            try:
+                os.remove(path)
+            except OSError as error:
+                print("Error: %s - %s." % (error.filename, error.strerror))
         else:
-            print("Sorry, I can not find %s file." % path)
-            return False
+            try:
+                shutil.rmtree(path, ignore_errors, onerror)
+            except OSError as error:
+                print('error %s' % error)
+                return False
+    else:
+        print("Sorry, I can not find %s file." % path)
+        return False
 
-        return True
-
-    @staticmethod
-    def print(*args):
-        try:
-            print(args)
-        except UnicodeEncodeError:
-            print('cannot print')
+    return True
