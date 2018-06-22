a259f5ab28c8d8fe84ecfc8b381d4cb473248f6a
kevin_slash <seng655615>
2018-06-22 11:13:25 +0700

    fix pytest with new update of preprocessing

--- pytest/preprocessing/test_preprocessing.py
+++ pytest/preprocessing/test_preprocessing.py
@@ -2,20 +2,19 @@ import sys, os
 import argparse
 import pytest
 
-syspath = 'slash-ml'
+syspath = 'khmer-ml'
 sys.path.append(os.path.abspath(os.path.join('..', syspath)))
 
 from khmerml.preprocessing.preprocessing_data import Preprocessing
 
 class TestPreprocessing:
   def test_loading_data(self):
-    # text_dir = 'data/dataset/text'
-    # result = Preprocessing().loading_data(text_dir, 'doc_freq', 25)
-    fname = os.getcwd() + '/data/dataset/matrix/doc_freq_25.csv'
+    text_dir = 'data/dataset/chatbot'
+    result = Preprocessing().loading_data(text_dir, 'doc_freq','all', 1)
+    fname = os.getcwd() + '/data/matrix/doc_freq_25.csv'
     assert os.path.isfile(fname)
     # pytest.xfail("not configured config")
 
   def test_loading_single_doc(self):
     result = Preprocessing().loading_single_doc('this is a famouse blockchain technology', 'doc_freq', 25)
     assert isinstance(result, list)
-        
--- pytest/preprocessing/test_read_content.py
+++ pytest/preprocessing/test_read_content.py
@@ -2,7 +2,7 @@ import sys, os
 import argparse
 import pytest
 
-syspath = 'slash-ml'
+syspath = 'khmer-ml'
 sys.path.append(os.path.abspath(os.path.join('..', syspath)))
 import collections
 from khmerml.preprocessing.read_content import ReadContent
@@ -10,9 +10,9 @@ from khmerml.preprocessing.read_content import ReadContent
 class TestPreprocessing:
 
   def test_load_content(self):
-    text_dir = 'data/dataset/text'
+    text_dir = 'data/dataset/chatbot'
     _words_all_articles, _all_words = ReadContent().load_content(text_dir)
-    assert  isinstance(_words_all_articles, list)
+    assert  isinstance(_words_all_articles, dict)
     assert  isinstance(_all_words, list)
 
   def test_remove_stopword(self):
@@ -22,10 +22,3 @@ class TestPreprocessing:
   def test_stemming_words(self):
     result = ReadContent().stemming_words( ['blockchain', 'famouse', 'good', 'future'])
     assert result == ['blockchain', 'fam', 'good', 'fut']
-
-  def test_merge_list_content(self):
-    result = ReadContent().merge_list_content([[['block', 'block', 'famouse', 'famouse', 'good', 'good', 'future']]])
-    compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
-    assert compare(result, ['block', 'famouse', 'good', 'future'])
-
-
